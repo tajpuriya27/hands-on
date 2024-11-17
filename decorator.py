a94b96d1c5af3cb4b1c0f10cd7a5ee6f2f6a1b47
tajpuriya27 <tajpuriyasunil27@gmail.com>
2024-11-17 23:30:28 +0545

    feat: decorator example

--- decorator.py
+++ decorator.py
@@ -1,16 +1,27 @@
 
-def decorate_greet(fx):
+def decorate_initial(fx):
+    # print('Running decorate_inital function')
     def wrapper():
-        print("Decorating greet")
+        print("Hello there!")
         fx()
-        print("Decorating greet done")
     return wrapper
+
+def decorate_after(fx):
+    # print('Running decorate_after function')
+    def wrapper():
+        fx()
+        print("Function Execution Completed!")
+    return wrapper
+
 
 ## Here, I am decorating the function. When this file is executed; at first this @decorate_greet will executed even if the greet function is not called.
 ## This is why we need wrapper and return the wrapper in decorate_greet function.
-@decorate_greet
-def greet():
-    print("Hello world")
+@decorate_after
+@decorate_initial
+def real_function():
+    print("My Real function Exectuion....")
 
 
-greet()
+real_function()
+
+
