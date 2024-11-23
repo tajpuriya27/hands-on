f1a584142037f66fad390d3c8454f5de776c2ea1
tajpuriya27 <tajpuriyasunil27@gmail.com>
2024-11-23 14:20:28 +0545

    nest: added query and it's type

--- nestjs-official/ilovecoffee/src/coffee/coffee.controller.ts
+++ nestjs-official/ilovecoffee/src/coffee/coffee.controller.ts
@@ -6,13 +6,20 @@ import {
   HttpStatus,
   Param,
   Post,
+  Query,
 } from '@nestjs/common';
 
+interface PaginationQuery {
+  limit: string;
+  offset: string;
+  test: string;
+}
 @Controller('coffee')
 export class CoffeeController {
   @Get()
-  getCoffee() {
-    return 'get all coffees';
+  getCoffee(@Query() paginationQuery: PaginationQuery) {
+    const { limit, offset, test } = paginationQuery;
+    return `get all coffees with limit ${limit} and offset ${offset}`;
   }
 
   @Get(':id')
