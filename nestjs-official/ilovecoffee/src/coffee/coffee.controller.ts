abc2b8020f3b792166f393c3de9d850eb2316c9b
tajpuriya27 <tajpuriyasunil27@gmail.com>
2024-11-23 14:12:55 +0545

    nest: video 10 completed

--- nestjs-official/ilovecoffee/src/coffee/coffee.controller.ts
+++ nestjs-official/ilovecoffee/src/coffee/coffee.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+} from '@nestjs/common';
 
 @Controller('coffee')
-export class CoffeeController {}
+export class CoffeeController {
+  @Get()
+  getCoffee() {
+    return 'get all coffees';
+  }
+
+  @Get(':id')
+  findOne(@Param() params) {
+    console.log('params', params);
+    return `here is ${params.id}`;
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.GONE)
+  createOne(@Body() body) {
+    console.log('here it comes ', body);
+    return `heeloo`;
+  }
+}
