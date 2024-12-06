be109ad4baa52c4f3be10e4b8ca2a5535c5d1bc4
tajpuriya27 <tajpuriyasunil27@gmail.com>
2024-12-07 00:38:31 +0545

    err: tailwind not working

--- react-router-dom/src/App.tsx
+++ react-router-dom/src/App.tsx
@@ -1,8 +1,10 @@
-import "./App.css";
+import Test from "./components/Test";
 
 function App() {
   return (
     <>
+      <h1 className="bg-blue-500 text-9xl">Testing text</h1>
+      <Test />
       <p>App Component</p>
     </>
   );
---
+++ react-router-dom/src/components/Test.tsx
@@ -0,0 +1,5 @@
+function Test() {
+  return <h1 className="text-7xl">Test</h1>;
+}
+
+export default Test;
