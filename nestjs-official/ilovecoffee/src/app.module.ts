e0aff6d7beeb4afde5caf1673f6e2d960d4033e9
tajpuriya27 <tajpuriyasunil27@gmail.com>
2024-12-18 13:57:48 +0545

    nest: added postgres connection

--- nestjs-official/ilovecoffee/src/app.module.ts
+++ nestjs-official/ilovecoffee/src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeeModule } from './coffee/coffee.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CoffeeModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'admin123',
+      database: 'ilovecoffee',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    CoffeeModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
